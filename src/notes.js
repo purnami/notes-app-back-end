6dc385b05435fde24cacd3f50af19a57d092a118
purnami <purnami.indryaswari@gmail.com>
2025-01-01 16:28:44 +0800

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+const notes = [
+//   {
+//     title: String,
+//     tags: [String],
+//     body: String,
+//     id: String,
+//     createdAt: String,
+//     updatedAt: String,
+//   },
+];
+
+
+module.exports = notes;
